ed461998fe2f5b3ed4600302f232b9d23fb84152
Samara Ferreira <samara.sofe@gmail.com>
2022-02-25 17:49:47 -0300

    To-do-list

---
+++ src/components/TaskDetails.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from './Button';
+
+ const TaskDetails = () => {
+     return ( 
+         <>
+            <div className='back-button-container'>
+                <Button>Voltar</Button>
+            </div>
+            <div className='task-details-container'>
+                <p></p>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda consequuntur tempore ex repellat incidunt accusamus temporibus porro. Est eveniet hic placeat excepturi optio itaque repudiandae. Quidem quae nobis vitae cum!
+                </p>
+            </div>
+             
+         </>
+      );
+ }
+  
+ export default TaskDetails;
